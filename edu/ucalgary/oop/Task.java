d73a3c70a8bb64ec8b9cb36c059275f6cd87c2cf
kaniz17 <kaniz.taiba@ucalgary.ca>
2023-04-10 02:14:04 -0400

    code for task

--- edu/ucalgary/oop/Task.java
+++ edu/ucalgary/oop/Task.java
@@ -1,20 +1,59 @@
 package edu.ucalgary.oop;
 
 public class Task {
-	private String description;
-	private String animal;
-	private int time;
-	Task(String desc, String ani, int t) {
-		this.description=desc;
-		this.animal=ani;
-		this.time=t;
-	}
-	/**Getters**/
-	public String getDescription() {return this.description;}
-	public String getAnimal() {return this.animal;}
-	public int getTime() {return this.time;}
-	/**Setters**/
-	public void setDescription(String desc) {this.description=desc;}
-	public void setAnimal(String ani) {this.animal=ani;}
-	public void setTime(int t) {this.time=t;}
+    private int taskID;
+    private String description;
+    private int duration;
+    private int maxWindow;
+
+    public Task(int taskID, String description, int duration, int maxWindow) {
+        setTaskID(taskID);
+        setDescription(description);
+        setDuration(duration);
+        setMaxWindow(maxWindow);
+    }
+
+    public int getTaskID() {
+        return taskID;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public int getMaxWindow() {
+        return maxWindow;
+    }
+
+    public void setTaskID(int taskID) throws IllegalArgumentException {
+        if (taskID < 0) {
+            throw new IllegalArgumentException("Invalid taskID");
+        }
+        this.taskID = taskID;
+    }
+
+    public void setDescription(String description) throws IllegalArgumentException {
+        if (description == null || description.isEmpty()) {
+            throw new IllegalArgumentException("Invalid description");
+        }
+        this.description = description;
+    }
+
+    public void setDuration(int duration) throws IllegalArgumentException {
+        if (duration < 0) {
+            throw new IllegalArgumentException("Invalid duration");
+        }
+        this.duration = duration;
+    }
+
+    public void setMaxWindow(int maxWindow) throws IllegalArgumentException {
+        if (maxWindow < 0) {
+            throw new IllegalArgumentException("Invalid maxWindow");
+        }
+        this.maxWindow = maxWindow;
+    }
 }
