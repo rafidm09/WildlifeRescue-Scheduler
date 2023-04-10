1c86edec2d67b3d5f84eef83b0604f88163cc537
NoshinZion <noshin.zion@ucalgary.ca>
2023-04-10 00:57:06 -0600

    Changed some stuff in the hour class

--- edu/ucalgary/oop/Hour.java
+++ edu/ucalgary/oop/Hour.java
@@ -2,19 +2,19 @@ package edu.ucalgary.oop;
 import java.util.ArrayList;
 
 public class Hour {
-	private ArrayList<Task> tasks = new ArrayList<Task>;
-	private int tSpent=0;
-	private int tAvail=60;
+	private ArrayList<Task> tasks = new ArrayList<Task>();
+	private int timeSpent=0;
+	private int timeAvailable=60;
 	private boolean backup=false;
 	/**Adds task and updates time values**/
 	public void addTask(Task t) {
-		int t = t.getTime;
-		this.tSpent+=t;
-		this.tAvail-=t;
-		if (this.tAvail<0) {
+		int time = t.getDuration();
+		this.timeSpent+=time;
+		this.timeAvailable-=time;
+		if (this.timeAvailable<0) {
 			if (backup) {
-				this.tSpent-=t;
-				this.tAvail+=t;
+				this.timeSpent-=time;
+				this.timeAvailable+=time;
 				//throw unavailable exception
 			} else {
 				//backup handler here
@@ -24,14 +24,14 @@ public class Hour {
 	}
 	/**Removes task at index and updates time values**/
 	public void removeTask(int i) {
-		t=this.tasks.get(i).getTime;
+		int time=this.tasks.get(i).getDuration();
 		this.tasks.remove(i);
-		this.tSpent-=t;
-		this.tAvail+=t;
+		this.timeSpent-=time;
+		this.timeAvailable+=time;
 	}
 	/**Getters**/
 	public Task getTask(int i) {return this.tasks.get(i);}
 	public ArrayList<Task> getTasks() {return this.tasks;}
-	public int[] getIme() {return {this.tSpent, this.tAvail};}
+	public int[] getIme() {return new int[]{this.timeSpent, this.timeAvailable};}
 	public boolean getBackup() {return this.backup;}
 }
