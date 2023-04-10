358bf0390321ae0f359ab1ef77f9f1f4479922db
J-Froese <josh.froese@ucalgary.ca>
2023-04-10 01:08:59 -0600

    main gui function done

--- edu/ucalgary/oop/GUI.java
+++ edu/ucalgary/oop/GUI.java
@@ -2,12 +2,13 @@ package edu.ucalgary.oop;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 import java.time.LocalDate;
 import java.time.Year;
 
-public class GUI {
+public class GUI implements ItemListener{
+	static JPanel cards;
+	static LocalDate date;
 	/**Reusable method for adding label and text fields in format: text ______ **/
 	public static TextField addTextEntry(String text, Container container) {
 		JPanel panel = new JPanel();
@@ -21,8 +22,10 @@ public class GUI {
 	}
 	/**Reuseable method for adding buttons with text as inputted text**/
     private static JButton addButton(String text, Container container) {
-        JButton button = new JButton(text);
-		container.add(button);
+        JPanel panel = new JPanel();
+		JButton button = new JButton(text);
+		panel.add(button);
+		container.add(panel);
 		return button;
 	}
 	/**Reuseable method for adding a number spinner with a label**/
@@ -41,95 +44,116 @@ public class GUI {
 	private static void popupBox(String info, String title) {
 		JOptionPane.showMessageDialog(null,info,"Popup: "+title,JOptionPane.INFORMATION_MESSAGE);
 	}
-	/**main application - contains date picker**/
-	private static void app(JFrame frame) {
-		EventQueue.invokeLater(() -> {
-			Container pane = frame.getContentPane();
-			pane.removeAll();
-			pane.setLayout(new BoxLayout(pane, BoxLayout.Y_AXIS));
-			frame.repaint();
-			JLabel label = new JLabel("Enter Date");
-			pane.add(label);
-			int y = Year.now().getValue();
-			JSpinner year = addSpin("Year:",y,y-15,y+15,pane);//Year is capped at the current year+-15
-			JSpinner mon = addSpin("Month:",1,1,12,pane);
-			JSpinner day = addSpin("Day:",1,1,31,pane);
-			JButton gen = addButton("Generate",pane);
-			gen.addActionListener(new ActionListener() {
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					int y = (Integer)year.getValue();
-					int m = (Integer)mon.getValue();
-					int d = (Integer)day.getValue();
-					LocalDate date = null;
-					try {
-						date = LocalDate.of(y,m,d);
-					} catch (Exception exc) {//catches invalid dates
-						popupBox("Invalid Date(y/m/d): "+y+"/"+m+"/"+d, "Invalid Date");
-					}
-					if (date!=null) {
-						//Generate schedule
-						genSchedule(frame, date);
-					}
+	/**Date picker**/
+	private static JPanel genDate() {
+		JPanel pane = new JPanel();
+		pane.setLayout(new BoxLayout(pane, BoxLayout.Y_AXIS));
+		
+		JLabel label = new JLabel("Enter Date");
+		pane.add(label);
+		int y = Year.now().getValue();
+		JSpinner year = addSpin("Year:",y,y-15,y+15,pane);//Year is capped at the current year+-15
+		JSpinner mon = addSpin("Month:",1,1,12,pane);
+		JSpinner day = addSpin("Day:",1,1,31,pane);
+		JButton gen = addButton("Generate",pane);
+		gen.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				int y = (Integer)year.getValue();
+				int m = (Integer)mon.getValue();
+				int d = (Integer)day.getValue();
+				date = null;
+				try {
+					date = LocalDate.of(y,m,d);
+				} catch (Exception exc) {//catches invalid dates
+					popupBox("Invalid Date(y/m/d): "+y+"/"+m+"/"+d, "Invalid Date");
 				}
-			});
-			
+				if (date!=null) {
+					CardLayout cl = (CardLayout) (cards.getLayout());
+					cl.show(cards, "App");
+				}
+			}
 		});
+		return pane;
 	}
 	/**Generates Schedule and contains display and download functionality**/
-	private static void genSchedule(JFrame frame, LocalDate date) {
-		EventQueue.invokeLater(() -> {
-			Container pane = frame.getContentPane();
-			pane.removeAll();
-			pane.setLayout(new BoxLayout(pane, BoxLayout.Y_AXIS));
-			frame.repaint();
-			JLabel label = new JLabel("Choose Display Method");
-			pane.add(label);
-			JButton disp = addButton("Display Schedule",pane);
-			JButton down = addButton("Download Schedule",pane);
-			disp.addActionListener(new ActionListener() {
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					//Display schedule
-					popupBox("Schedule disp","Display");
-				}
-			});
-			down.addActionListener(new ActionListener() {
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					//Download schedule
-					popupBox("Schedule downloaded","Download");
-				}
-			});
-			
-		});
-	}
-	/**Opens new window with text of schedule shown**/
-	private static void dispSchedule(JFrame frame) {
-		
-	}
-	/**Startup GUI and database connection**/
-	public static void main(String[]args) {
-		EventQueue.invokeLater(() -> {
-		JFrame frame = new JFrame("Schedule Generator");
-		frame.setSize(300, 400);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		Container pane = frame.getContentPane();
+	private static JPanel genApp() {
+		JPanel pane = new JPanel();
 		pane.setLayout(new BoxLayout(pane, BoxLayout.Y_AXIS));
-		//genSchedule(frame,null);//for testing
-		/**
+		
+		JLabel label = new JLabel("Choose Display Method");
+		pane.add(label);
+		JButton disp = addButton("Display Schedule",pane);
+		JButton down = addButton("Download Schedule",pane);
+		JButton back = addButton("Pick new Date",pane);
+		disp.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				//Display schedule
+				popupBox("Schedule disp","Display");
+			}
+		});
+		down.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				//Download schedule
+				popupBox("Schedule downloaded","Download");
+			}
+		});
+		back.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				CardLayout cl = (CardLayout) (cards.getLayout());
+				cl.show(cards, "Date");				
+			}
+		});
+		return pane;
+	}
+	/**connects to database**/
+	private static JPanel genConn() {
+		JPanel pane = new JPanel();
+		pane.setLayout(new BoxLayout(pane, BoxLayout.Y_AXIS));
+		
 		TextField user = addTextEntry("Enter User",pane);
 		TextField pass = addTextEntry("Enter Passkey",pane);
 		JButton login = addButton("Login",pane);
 		login.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				//DB connection here
-				//genSchedule(frame,null);
+				CardLayout cl = (CardLayout) (cards.getLayout());
+				cl.show(cards, "Date");
 			}
 		});
-		**/
-		frame.setVisible(true);
+		return pane;
+	}
+	/**Startup**/
+	public static void start() {
+		EventQueue.invokeLater(() -> {
+			JFrame frame = new JFrame("Schedule Generator");
+			frame.setSize(300, 400);
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			Container pane = frame.getContentPane();
+			
+			JPanel card1 = genConn();
+			JPanel card2 = genDate();
+			JPanel card3 = genApp();
+			
+			cards = new JPanel(new CardLayout());
+			cards.add(card1, "Connect");
+			cards.add(card2, "Date");
+			cards.add(card3, "App");
+			
+			pane.add(cards, BorderLayout.CENTER);
+			
+			frame.setVisible(true);
 		});
+	}
+	/**Startup**/
+	public static void main(String[]args) {
+		start();
+	}
+	public void itemStateChanged(ItemEvent evt) {
+		CardLayout cl = (CardLayout)(cards.getLayout());
+		cl.show(cards, (String)evt.getItem());
 	}
 }
