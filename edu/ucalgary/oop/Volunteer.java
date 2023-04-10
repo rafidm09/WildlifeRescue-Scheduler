e2c8236b37dd23f03ee5a37f88889f945edc43b5
NoshinZion <noshin.zion@ucalgary.ca>
2023-04-10 00:29:02 -0600

    Added the interface volunteer

--- edu/ucalgary/oop/Volunteer.java
+++ edu/ucalgary/oop/Volunteer.java
@@ -1,11 +1,5 @@
 package edu.ucalgary.oop;
 
-public class Volunteer {
-	private String name;
-	private String availability;
-	public Volunteer(String name, String avail) {
-		this.name = name;
-		this.availability = avail;
-	}
-	
+public interface Volunteer {
+	public boolean backupVolunteer();
 }
