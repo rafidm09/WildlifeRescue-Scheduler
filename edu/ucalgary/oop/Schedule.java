0c01d94c21eed3c654a7491914b597a1310dbc3e
rafidm09 <113808092+rafidm09@users.noreply.github.com>
2023-04-10 22:46:00 -0600

    Beginning schedule builder

--- edu/ucalgary/oop/Schedule.java
+++ edu/ucalgary/oop/Schedule.java
@@ -1,5 +1,19 @@
 package edu.ucalgary.oop;
 
+import java.util.HashMap;
 
+class Schedule {
+    HashMap<String, String> schedule = new HashMap<>();
 
+    public Schedule() {
 
+    }
+
+    public void makeSchedule() {
+
+    }
+
+    public HashMap<String, String> getSchedule() {
+        return this.schedule;
+    }
+}
